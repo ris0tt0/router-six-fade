7faee5bdbefc24b05454406e4a83ec874147932d
Jonathan Gee <j@jonathangee.com>
2025-06-05 20:16:26 -0700

    feat: add players component

--- packages/rsix-fe-spa/src/routes/choose/index.tsx
+++ packages/rsix-fe-spa/src/routes/choose/index.tsx
@@ -1,12 +1,42 @@
+import { Player } from '@jsix/be-db';
 import { styled } from '@mui/material';
-import React, { FC } from 'react';
+import React, { FC, useEffect, useMemo, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store/redux';
 
 const ChoosePlayersContainerStyled = styled('div')`
   display: flex;
-  padding: 20px;
+  flex-direction: column;
+  padding: 10px;
   border: 1px red solid;
+
+  h3 {
+    border: 1px yellow solid;
+  }
 `;
 
 export const ChoosePlayer: FC = () => {
-  return <ChoosePlayersContainerStyled>choose</ChoosePlayersContainerStyled>;
+  const playerData = useSelector<RootState>((state) => state.app.players);
+  const [players, setPlayers] = useState<Player[] | null>(null);
+
+  useEffect(() => {
+    if (playerData) {
+      const result = Object.values(playerData) ?? null;
+      setPlayers(result);
+    }
+  }, [playerData]);
+
+  const items = useMemo(() => {
+    const result = players?.map((player) => {
+      return <div key={player.id}>{player.name}</div>;
+    });
+    return result;
+  }, [players]);
+
+  return (
+    <ChoosePlayersContainerStyled>
+      <h3>select a player</h3>
+      <div>{items}</div>
+    </ChoosePlayersContainerStyled>
+  );
 };
